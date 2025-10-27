1b05f7da6fa7f8638442312e7ce8fe0cfad3207a
Simo Tukiainen <simo.tukiainen@fmi.fi>
2025-10-27 11:33:07 +0200

    Add support for L2 products

--- earthcare_downloader/metadata.py
+++ earthcare_downloader/metadata.py
@@ -5,6 +5,7 @@ import aiohttp
 from earthcare_downloader import utils
 
 Prod = Literal[
+    # L1 products
     "ATL_NOM_1B",
     "AUX_JSG_1D",
     "BBR_NOM_1B",
@@ -12,6 +13,25 @@ Prod = Literal[
     "CPR_NOM_1B",
     "MSI_NOM_1B",
     "MSI_RGR_1C",
+    # L2 products
+    "AC__TC__2B",
+    "AM__ACD_2B",
+    "AM__CTH_2B",
+    "ATL_ARE_2A",
+    "ATL_ALD_2A",
+    "ATL_CTH_2A",
+    "ATL_EBD_2A",
+    "ATL_FM__2A",
+    "ATL_ICE_2A",
+    "ATL_TC__2A",
+    "BM__RAD_2B",
+    "CPR_CD__2A",
+    "CPR_CLD_2A",
+    "CPR_FMR_2A",
+    "CPR_TC__2A",
+    "MSI_AOT_2A",
+    "MSI_CM__2A",
+    "MSI_COP_2A",
 ]
 
 
@@ -20,7 +40,10 @@ async def get_files(
 ) -> list[str]:
     lat_buffer = utils.distance_to_lat_deg(distance)
     lon_buffer = utils.distance_to_lon_deg(lat, distance)
-    url = "https://ec-pdgs-discovery.eo.esa.int/socat/EarthCAREL1Validated/search"
+    level = "2" if "2" in product else "1"
+    url = (
+        f"https://ec-pdgs-discovery.eo.esa.int/socat/EarthCAREL{level}Validated/search"
+    )
     query_params = {
         "service": "SimpleOnlineCatalogue",
         "version": "1.2",
