29b877204b8a8e6ac7c1f5b0cf3e257f294921f9
Simo Tukiainen <simo.tukiainen@fmi.fi>
2025-11-25 10:07:53 +0200

    Improve docstrings

--- earthcare_downloader/api.py
+++ earthcare_downloader/api.py
@@ -28,20 +28,23 @@ def search(
         product: Product type to search for.
         start: Start date (inclusive) for data search. Default is mission start date.
         stop: Stop date (inclusive) for data search. Default is today's date.
-        date: Single date for data search. Can be used instead of --start and --stop.
+        date: Single date for data search. Overrides start and stop.
         orbit_min: Minimum orbit number. Default is 0.
         orbit_max: Maximum orbit number. Default is None (no limit).
-        orbit: Single orbit number. Can be used instead of orbit_min and orbit_max.
-        lat: Latitude of the center point.
-        lon: Longitude of the center point.
+        orbit: Single orbit number. Overrides orbit_min and orbit_max.
+        lat: Latitude of the center point (-90 to 90 degrees).
+        lon: Longitude of the center point (-180 to 180 degrees).
         radius: Search radius in kilometers. Default is whole Earth.
+            Use with lat and lon.
         lat_range: Tuple specifying the latitude range (min_lat, max_lat).
+            Use with lon_range.
         lon_range: Tuple specifying the longitude range (min_lon, max_lon).
+            Use with lat_range.
         all: Whether to search for all versions (different baselines and/or
             processing runs) of the product. Default is False.
 
     Returns:
-        List of URLs of the matching data files.
+        List of File objects.
     """
     return asyncio.run(
         aio.search(
@@ -71,10 +74,10 @@ def download(
     quiet: bool | None = None,
     credentials: tuple[str, str] | None = None,
 ) -> list[Path]:
-    """Download EarthCARE data files from the given URLs.
+    """Download EarthCARE data files from the given File objects.
 
     Args:
-        files: List of URLs to download.
+        files: List of File objects to download.
         output_path: Directory to save the downloaded files. Default is current
             directory.
         by_product: Whether to create subdirectories for each product type.
